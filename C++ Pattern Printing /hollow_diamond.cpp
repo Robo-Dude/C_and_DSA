1991eb44b5516bf38c0cedbc77a74ba194dfbd7b
Robo-Dude <sourabhtiwari10397@gmail.com>
2026-02-28 23:06:05 +0530

    day14

--- C++ Pattern Printing /hollow_diamond.cpp
+++ C++ Pattern Printing /hollow_diamond.cpp
@@ -27,7 +27,47 @@ int main(){
 
     int n;
 
-    cout << "enter: ";
+    cout << "Enter: ";
 
+    cin >> n;
 
+    for(int i = 1; i <= n; i++){
+
+        for(int j = 1; j <= n + i - 1; j++){
+
+            if (j == (n - i + 1)){
+
+                cout << "* ";
+            }
+            else if ( j == (n + i - 1)){
+
+                cout << "* ";
+            }
+            else{
+
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    for(int i = n - 1; i >= 1; i--){
+
+        for(int j = 1; j <= n + i - 1; j++){
+
+            if (j == (n - i + 1)){
+
+                cout << "* ";
+            }
+            else if ( j == (n + i - 1)){
+
+                cout << "* ";
+            }
+            else{
+
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
 }
